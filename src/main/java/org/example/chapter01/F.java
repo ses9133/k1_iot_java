801c156fcb375de186af31245238c921f0727423
JEONG-EUN-HYE1 <ses9133@gmail.com>
2025-07-01 17:39:58 +0900

    2025/07/01

---
+++ src/main/java/org/example/chapter01/F.java
@@ -0,0 +1,10 @@
+package org.example.chapter01;
+
+public class F {
+    public static void main(String[] args) {
+        int num1 = 10;
+        double num2 = 3.5;
+        double result = num1 + num2;
+        System.out.println(result);
+    }
+}
