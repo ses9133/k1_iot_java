582c4cea8ca20e14b87f57fe76ef815366f4222c
ses9133 <ses9133@naver.com>
2025-07-15 09:50:57 +0900

    20250715 git conflict merge

--- src/main/java/org/example/chapter12/model/Student.java
+++ src/main/java/org/example/chapter12/model/Student.java
@@ -42,43 +42,4 @@ public class Student {
     public String toString() {
         return "Student { ID= " + studentId + "학생 이름= " + name + ", 나이= " + age + ", 학번= " + studentNumber + ", 생성일= " + createdAt + " }";
     }
-
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getStudentNumber() {
-        return studentNumber;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{ " +
-                "ID=" + studentId +
-                ", 이름=" + name +
-                ", 나이=" + age +
-                ", 학번=" + studentNumber +
-                ", 생성일=" + createdAt + " }";
-    }
 }
